0cdb15b18855513ef6a6647af52a2e087659d6da
KennethEvolves <kennethgaeliuitrios02@gmail.com>
2024-04-20 13:37:31 -0600

    added responsive design to Features.jsx component

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -14,26 +14,23 @@ const Card = ({ data }) => {
 
 const Features = () => {
     return (
-        <div className="w-[1080px] h-[348px] pb-[50px] flex flex-r justify-center items-center">
-            <div className="w-[1033px] h-[248px] flex flex-r justify-center items-center gap-[110px]">
-                <Card data={{
-                    image: 'cards-icon.png',
-                    title: 'Customizable card',
-                    description: 'Custom your own card for your exact incomes and expenses needs.'
-                }} />
-                <Card data={{
-                    image: 'coin-icon.png',
-                    title: 'No payment fee',
-                    description: 'Transfer your payment all over the world with no payment fee.'
-                }} />
-                <Card data={{
-                    image: 'purse-icon.png',
-                    title: 'All in one place',
-                    description: 'The right place to keep your credit and debit cards, boarding passes & more.'
-                }} />
-            </div>
+        <div className="w-[1080px] h-[348px] p-[50px] mb-[80px] flex flex-r justify-center items-center gap-[110px] max-lg:w-min max-lg:h-min max-lg:flex-col">
+            <Card data={{
+                image: 'cards-icon.png',
+                title: 'Customizable card',
+                description: 'Custom your own card for your exact incomes and expenses needs.'
+            }} />
+            <Card data={{
+                image: 'coin-icon.png',
+                title: 'No payment fee',
+                description: 'Transfer your payment all over the world with no payment fee.'
+            }} />
+            <Card data={{
+                image: 'purse-icon.png',
+                title: 'All in one place',
+                description: 'The right place to keep your credit and debit cards, boarding passes & more.'
+            }} />
         </div>
-
     )
 }
 
