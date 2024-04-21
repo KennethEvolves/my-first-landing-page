60ddf7ef2a9d86261a61709ef518e9d28bb8c78d
KennethEvolves <kennethgaeliuitrios02@gmail.com>
2024-04-21 09:32:13 -0600

    added responsive design to Logos.jsx component

--- src/components/Logos.jsx
+++ src/components/Logos.jsx
@@ -1,13 +1,14 @@
 
 const Logos = () => {
     return (
-
-        <div className="w-1080 h-170 flex flex-r justify-center items-center border-y-2 border-y-secondarycolor">
-            <div className="w-931">
+        <div className="w-1080 h-170 flex flex-r justify-center items-center border-y-2 border-y-secondarycolor max-md:w-[340px] max-lg:w-[700px]">
+            <div className="w-[310px] h-[79px]">
+                <img src="logos2.png" />
+            </div>
+            <div className="w-931 h-[90px] flex flex-r justify-center items-center bg-primarycolor absolute max-lg:hidden">
                 <img src="logos.png" />
             </div>
         </div>
-
     )
 }
 
